c58ea154aae36e880615b92d5afb65ac02952198
Francisco Lopez <loaf.85@gmail.com>
2022-10-02 21:16:14 -0700

    Reactive Data with the Composition API

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
   <h1>{{ name }}</h1>
   <input type="text" v-model="name">
   <button @click="placeOrder">Place order</button>
-  <YummyMeal name="Hamburger 🍔" :price="5" @addToCart="addItemToCart" />
+  <YummyMeal :name="meal.name" :price="meal.price" @addToCart="addItemToCart" />
 </template>
 
 <script>
 import YummyMeal from "./components/YummyMeal.vue";
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 
 export default {
   components: {YummyMeal},
   setup() {
     const name = ref("The Snazzy Burger");
-    console.log(name)
+    const meal = reactive({ name: "Hamburger 🍔", price: 5});
     const placeOrder = () => alert('order placed!');
     const addItemToCart = (item) => alert(`One ${item} added to the cart!`)
-    return {name, placeOrder, addItemToCart}
+    return {name, placeOrder, addItemToCart, meal}
   }
 }
 </script>
