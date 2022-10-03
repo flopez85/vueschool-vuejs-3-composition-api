1326e0dd0a6fa352cbad1e25cb6ae5dcdc5aeb29
Francisco Lopez <loaf.85@gmail.com>
2022-10-02 22:24:03 -0700

    Using Watch with the Composition API part 2

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <h1>{{ name }}</h1>
   <input type="text" v-model="name">
   <button @click="placeOrder">Place order</button>
-  <YummyMeal :name="meal.name" :price="meal.price" @addToCart="addItemToCart" />
+  <button @click="removeWatcher">Remove Watcher</button>
+  <YummyMeal
+      v-for="meal in meals"
+      :name="meal.name"
+      :price="meal.price"
+      @addToCart="addItemToCart"
+  />
 </template>
 
 <script>
@@ -13,13 +19,20 @@ export default {
   components: {YummyMeal},
   setup() {
     const name = ref("The Snazzy Burger");
+    const cart = reactive([])
     const meal = reactive({name: "Hamburger 🍔", price: 5});
+    const meals = reactive([
+      {name: "Hamburger 🍔", price: 5},
+      {name: "Cheeseburger 🧀", price: 6},
+      {name: "Impossible Burger 🥕", price: 7},
+      {name: "Fries 🍟", price: 7},
+    ])
     const placeOrder = () => alert('order placed!');
-    const addItemToCart = (item) => alert(`One ${item} added to the cart!`);
-    watch(name, (newName, oldName) => console.log(newName, oldName), {
-      immediate: true
-    })
-    return {name, placeOrder, addItemToCart, meal}
+    const addItemToCart = (item) => cart.push(item);
+
+    const removeWatcher = watch([name, () => [...cart]], (newValue, oldValue) => console.log(newValue, oldValue))
+
+    return {name, placeOrder, addItemToCart, meal, meals, removeWatcher}
   }
 }
 </script>
