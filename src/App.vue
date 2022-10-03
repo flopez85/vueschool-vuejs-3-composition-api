4046e8f20814f81fc42daa8a0ad7a01d8d82a5eb
Francisco Lopez <loaf.85@gmail.com>
2022-10-02 20:40:30 -0700

    The Setup Method's Parameters: props and context

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <h1>{{ name }}</h1>
   <button @click="placeOrder">Place order</button>
+  <YummyMeal name="Hamburger 🍔" :price="5" @addToCart="addItemToCart" />
 </template>
 
 <script>
+import YummyMeal from "./components/YummyMeal.vue";
+
 export default {
+  components: {YummyMeal},
   setup() {
     const name = "The Snazzy Burger";
-    const placeOrder = () => alert('order placed!')
-    return { name, placeOrder }
+    const placeOrder = () => alert('order placed!');
+    const addItemToCart = (item) => alert(`One ${item} added to the cart!`)
+    return {name, placeOrder, addItemToCart}
   }
 }
 </script>
---
+++ src/components/YummyMeal.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>
+    <strong>{{ name }}</strong>
+    {{ price }}
+    <button @click="addToCart">Add to Cart</button>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    price: Number
+  },
+  setup(props, {attrs, slots, emit}) {
+    const addToCart = () => emit("addToCart", props.name);
+
+    return {
+      addToCart
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
